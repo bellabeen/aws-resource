08848a8fda1a88dcecacdaec4c6f48356a7ad591
bellabeen <rootsquid@gmail.com>
2024-03-18 08:52:10 +0700

    [ADD] lambda upgrade & downgrade ec2 instance type

---
+++ lambda/ec2-downgrade-intance-type.py
@@ -0,0 +1,14 @@
+import boto3
+
+# Create Schedule on Eventbridge
+
+def lambda_handler(event, context):
+    client = boto3.client('ec2')
+    # Insert your Instance ID here
+    my_instance = 'i-0ed7dcebbbf30517d'  # Stop the instance
+    client.stop_instances(InstanceIds=[my_instance])
+    waiter = client.get_waiter('instance_stopped')
+    waiter.wait(InstanceIds=[my_instance])  # Change the instance type
+    client.modify_instance_attribute(InstanceId=my_instance,
+    Attribute='instanceType', Value='r6i.large')  # Start the instance
+    client.start_instances(InstanceIds=[my_instance])
